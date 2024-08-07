090b959df7094042584c83642cc0e4c76c2cd8f7
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2024-08-07 16:43:33 -0300

    feat(setup): setup git and version control.

---
+++ backend/src/test/java/com/maximiliano/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximiliano.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
